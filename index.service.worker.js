24898be089137ddd8c6ee25139ab65409b72ce3a
Devy4304 <devin.lisiecki@icloud.com>
2024-05-02 15:31:46 -0700

    e

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1714660775|82925949";
+const CACHE_VERSION = "1714688985|113445311";
 const CACHE_PREFIX = "Test-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
